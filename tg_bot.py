0783b8a1c672fc7861514b0ff97d68e5b9b9c341
Aleksei Lobachev <90100576+LexLobachev@users.noreply.github.com>
2023-08-04 10:13:11 +0400

    add DialogFlow to VK bot

--- tg_bot.py
+++ tg_bot.py
@@ -4,23 +4,23 @@ from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 from decouple import config
 
-from run import detect_intent_texts
+from intent_detector import detect_intent_texts
 
 logging.basicConfig(level=logging.INFO)
 
 logger = logging.getLogger(__name__)
 
 
-def start(update: Update, context: CallbackContext) -> None:
+def start(update: Update, context: CallbackContext):
     user = update.effective_user
     update.message.reply_text('Здравствуйте!')
 
 
-def help_command(update: Update, context: CallbackContext) -> None:
+def help_command(update: Update, context: CallbackContext):
     update.message.reply_text('Help!')
 
 
-def echo(update: Update, context: CallbackContext) -> None:
+def reply(update: Update, context: CallbackContext):
     update.message.reply_text(detect_intent_texts(project_id, session_id, update.message.text, 'ru-Ru'))
 
 
@@ -32,7 +32,7 @@ def main():
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", help_command))
 
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, reply))
 
     updater.start_polling()
 
--- vk_bot.py
+++ vk_bot.py
@@ -3,27 +3,26 @@ import random
 import vk_api as vk
 from vk_api.longpoll import VkLongPoll, VkEventType
 from decouple import config
+from intent_detector import detect_intent_texts
 
 
-def echo(event, vk_api):
+def reply(event, vk_api, project_id):
     vk_api.messages.send(
         user_id=event.user_id,
-        message=event.text,
+        message=detect_intent_texts(project_id, event.user_id, event.text, 'ru'),
         random_id=random.randint(1, 1000)
     )
 
 
 def main():
+    project_id = config("PROJECT_ID")
     vk_session = vk.VkApi(token=config("VK_TOKEN"))
     vk_api = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
 
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            print('Новое сообщение:')
-            if event.to_me:
-                print('Для меня от: ', event.user_id)
-                echo(event, vk_api)
+            reply(event, vk_api, project_id)
 
 
 if __name__ == '__main__':
